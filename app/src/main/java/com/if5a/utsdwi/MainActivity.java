23b2f28202c57a7747b70b9bd38801bad960eeb6
Dwi Apriyanti Sastika <sastikadwiapriyanti2@mhs.mdp.ac.id>
2022-10-31 22:00:58 +0700

    22.00

--- app/src/main/java/com/if5a/utsdwi/MainActivity.java
+++ app/src/main/java/com/if5a/utsdwi/MainActivity.java
@@ -3,14 +3,15 @@ package com.if5a.utsdwi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    Button BtnDaftar;
-    EditText etNama;
-    EditText etNomor;
+    private EditText etnamalengkap, etnomorpendaftaran;
+    private Spinner spjalurpendaftaran;
+    private Button btndaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("uts Dwi Apriyanti Sastika");
 
+        btndaftar = findViewById(R.id.btn_daftar);
+        etnamalengkap = findViewById((R.id.et_namalengkap));
+        etnomorpendaftaran = findViewById(R.id.et_nomorpendaftaran);
 
     }
 }
